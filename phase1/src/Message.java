cc4fcf9facdf656fc182fecc1bc49e44c49798dc
PRB27 <peter.bilski.1@gmail.com>
2020-11-01 13:18:45 -0500

    Added entities v2

---
+++ phase1/src/Message.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Message {
+    private String sender;
+    private String recipient;
+    private ArrayList<Message> replies;
+    private String id;
+    public Message(String sender, String recipient, String id){
+
+    }
+}
