d9e67742a0585086d2cde0175a712671ede9d3ce
aribshaikh <aribshaikh10@gmail.com>
2020-11-04 01:02:43 -0500

    Implemented Event class(entity)

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Event {
+    private double eventTime;
+    private ArrayList<Event> attendeeList;
+    private String speakerName;
+
+    public Event(String speakerName, double eventTime, ArrayList<Event>attendeeList){
+        this.speakerName = speakerName;
+        this.eventTime = eventTime;
+        this.attendeeList = attendeeList;
+
+    }
+
+    public String getSpeakerName() {
+        return speakerName;
+    }
+
+    public double getEventTime() {
+        return eventTime;
+    }
+
+    public ArrayList<Event> getAttendeeList() {
+        return attendeeList;
+    }
 }
