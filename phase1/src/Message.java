d14c077b52f82a6bc338c23653f19a5ae7bae8df
PRB27 <peter.bilski.1@gmail.com>
2020-11-05 11:35:58 -0500

    Finished Message and MessageManager until we think of what to do with Users

--- phase1/src/Message.java
+++ phase1/src/Message.java
@@ -1,30 +1,40 @@
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Message {
     private String sender;
-    private String recipient;
+    private ArrayList<String> recipients = new ArrayList<String>();
+    private String content;
+
     private ArrayList<Message> replies;
     private String id;
+    private LocalDateTime time;
 
-    public Message(String sender, String recipient, String id){
+    public Message(String sender, String recipient, String content, String id){
         this.sender = sender;
-        this.recipient = recipient;
+        this.recipients.add(recipient);
+        this.content = content;
         this.id = id;
+        this.time = LocalDateTime.now();
     }
 
-    public String getSender(){
-        return sender;
+    public Message(String sender, ArrayList<String> recipients, String content, String id){
+        this.sender = sender;
+        this.recipients.addAll(recipients);
+        this.content = content;
+        this.id = id;
+        this.time = LocalDateTime.now();
     }
 
-    public String getRecipient(){
-        return recipient;
-    }
+    public String getSender(){ return sender; }
+
+    public ArrayList<String> getRecipients(){ return recipients; }
 
     public ArrayList<Message> getReplies (){
         return replies;
     }
 
-    public String getId(){
-        return id;
-    }
+    public String getId(){ return id; }
+
 }
