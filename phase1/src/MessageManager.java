fa30256c57955cb0436b77fa776d76a02d68edcb
PRB27 <peter.bilski.1@gmail.com>
2020-11-07 14:54:07 -0500

    Added the class/method that makes unique IDs

--- phase1/src/MessageManager.java
+++ phase1/src/MessageManager.java
@@ -3,15 +3,16 @@ import java.util.ArrayList;
 public class MessageManager {
     private ArrayList<Message> allMessages;
 
-    public void sendMessageSingle(String senderId, String recipientId, String content){
-        Message message = new Message(senderId, recipientId, content, "0");
+    public Message sendMessageSingle(String senderId, String recipientId, String content){
+        return new Message(senderId, recipientId, content, "0");
     }
 
-    public void sendMessageMulti(String senderId, ArrayList<String> recipientIds, String content){
-        Message message = new Message(senderId, recipientIds, content, "0");
+    public Message sendMessageMulti(String senderId, ArrayList<String> recipientIds, String content){
+        return new Message(senderId, recipientIds, content, "0");
     }
 
     public void addReply(Message originalMessage, Message reply){
         originalMessage.getReplies().add(reply);
     }
+
 }
