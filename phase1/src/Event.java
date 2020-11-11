a7060234cc3adedbc79abc2b5c5528b506da8230
aribshaikh <aribshaikh10@gmail.com>
2020-11-11 12:32:17 -0500

    Added eventname to event class

--- phase1/src/Event.java
+++ phase1/src/Event.java
@@ -1,17 +1,23 @@
 import java.util.ArrayList;
 
 public class Event {
+    private String eventName;
     private String speakerName;
     private double eventTime;
     private int roomNumber;
     private ArrayList<User> attendeeList;
 
-    public Event(String speakerName, double eventTime, int roomNumber, ArrayList<User>attendeeList){
+    public Event(String eventName, String speakerName, double eventTime, int roomNumber, ArrayList<User>attendeeList){
+        this.eventName = eventName;
         this.speakerName = speakerName;
         this.eventTime = eventTime;
         this.roomNumber = roomNumber;
         this.attendeeList = attendeeList;
 
+    }
+
+    public String getEventName() {
+        return eventName;
     }
 
     public String getSpeakerName() {
@@ -28,6 +34,10 @@ public class Event {
 
     public ArrayList<User> getAttendeeList() {
         return attendeeList;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
     }
 
     public void setSpeakerName(String speakerName) {
