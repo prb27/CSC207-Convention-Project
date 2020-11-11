79778c520b67fd8841b9b1f13c10af07d2a333f0
Khoa Pham <khoat.pham@mail.utoronto.ca>
2020-11-11 15:00:34 -0500

    Nov 11, 2020 - 15:00

--- phase1/src/AttendeeMenu.java
+++ phase1/src/AttendeeMenu.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+
 public class AttendeeMenu {
     private final AttendeeManager attendeeManager = new AttendeeManager();
-    public void messAllContacts(String a, String content) {
+    private MessageManager messageManager;
+
+
+    public AttendeeMenu(MessageManager messageManager) {
+        this.messageManager = messageManager;
+    }
+
+    public void messAllContacts(String a, String content, int convoNumber) {
         Attendee attendee = attendeeManager.getAttendee(a);
-        //MessageManager.sendMessageMulti(a, attendee.getContacts(), content);
+        messageManager.sendMessageMulti(a, attendee.getContacts(), content, convoNumber);
+    }
+
+    public void messSome(String a, ArrayList<String> receivers, String content, int convoNumber) {
+        messageManager.sendMessageMulti(a, receivers, content, convoNumber);
+    }
+
+    public void messOne(String a, String b, String content, int convoNumber) {
+        messageManager.sendMessageSingle(a, b, content, convoNumber);
     }
 }
