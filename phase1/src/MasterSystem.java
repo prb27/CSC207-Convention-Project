4a8980b347803b89a6a51b56483a37b051a03093
akshatayush <akshatayush07@gmail.com>
2020-11-15 21:49:06 +0530

    User can login in MasterSystem

--- phase1/src/MasterSystem.java
+++ phase1/src/MasterSystem.java
@@ -1,4 +1,21 @@
 public class MasterSystem {
 
+    public void run() {
+        boolean loggedIn = false;
+        String username;
+        String password;
+        String accountType;
+        while(!loggedIn) {
+            TextUI.loginScreen();
+            username = TextUI.input();
+            password = TextUI.input();
+            accountType = TextUI.input();
+            if(AccountHandler.login(username, password, accountType))
+                loggedIn = true;
+            else
+                TextUI.incorrectLogin();
+        }
+    }
 
+    public void commandHandler(String command, String menu)
 }
