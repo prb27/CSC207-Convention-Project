1d9ade2d4ab5e2319a049814c45c15c578f7ab8e
aribshaikh <aribshaikh10@gmail.com>
2020-11-14 19:59:33 -0500

    Implemented addEvent, removeEvent, removeAttendee, getEvent for the EventManager class.

--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EventManager {
 
@@ -8,30 +9,62 @@ public class EventManager {
         return EventList;
     }
 
-    public void addEvent(String eventName, String speakerName, double eventTime, int roomNumber, ArrayList<User>attendeeList){
-        //First check if speakername is available with giventime
-        //Check roomNumber and Time
-        //Create new event
-        //Add it to EventList
-        //Add it to SpeakerTalk
+    public void addEvent(String eventName, String eventTime, String roomNumber, ArrayList<User>attendeeList, String speakerName, HashMap<String, String> listOfTalks){
+        //Will use the following of Speakermanager object is passed
+        //HashMap<String, String> listOfTalks = null;
+        //listOfTalks = speaker.getListOfTalks();
+
+        boolean check = false;
+            if (listOfTalks.containsKey(eventTime)){
+                System.out.println("Speaker time conflict; cannot reserve another talk");
+
+            }
+
+            else{
+                for (Event event: EventList ){
+                    if(event.getRoomNumber().equals(roomNumber) && event.getEventTime().equals(eventTime)){
+                        check = true;
+                    }
+                }
+                if (!check){
+                    Event newEvent = new Event(eventName, speakerName, eventTime, roomNumber, attendeeList);
+                    EventList.add(newEvent);
+                    listOfTalks.put(eventTime, eventName);
+                }
+            }
 
     }
 
-    public void removeEvent(String eventName, String speakerName, double eventTime, int roomNumber, ArrayList<User>attendeeList){
-        //Iterate through eventlist, remove object
-        //Remove it from speaker talk
+    public void removeEvent(Event event, HashMap<String, String> listOfTalks ){
+
+        EventList.remove(event);
+        listOfTalks.remove(event.getEventTime(),event.getSpeakerName());
     }
 
-    public void reserveAttendee(){
+    public void reserveAttendee(Event event, Attendee attendee){
+        //Need to check if attendee is not already registered for event at this time
+    }
+
+    public void removeAttendee(Event event, Attendee attendee){
+        event.getAttendeeList().remove(attendee);
 
     }
 
-    public void removeAttendee(){
+    public Event getEvent(String eventName){
+
+        for(Event event: EventList){
+            if(event.getEventName().equals(eventName)) {
+                return event;
+            }
+        }
+
+        return null;
 
     }
-
 
 }
+
+
 
 
 
