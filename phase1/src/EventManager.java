d0d8b3baa47317f2e0d02e3a28326cf37e30c8ff
aribshaikh <aribshaikh10@gmail.com>
2020-11-16 20:58:46 -0500

    Added edge-case error checks for EventManager

--- phase1/src/EventManager.java
+++ phase1/src/EventManager.java
@@ -9,15 +9,14 @@ public class EventManager {
         return EventList;
     }
 
-    public boolean addEvent(String eventName, String eventTime, String roomNumber, ArrayList<String>attendeeList, String speakerName, HashMap<String, String> listOfTalks){
+    public String addEvent(String eventName, String eventTime, String roomNumber, ArrayList<String>attendeeList, String speakerName, HashMap<String, String> listOfTalks){
         //Will use the following of Speakermanager object is passed
         //HashMap<String, String> listOfTalks = null;
         //listOfTalks = speaker.getListOfTalks();
 
         boolean trigger = true;
-        boolean check = false;
         if (listOfTalks.containsKey(eventTime)){
-                System.out.println("Speaker time conflict; cannot reserve another talk");
+                return("STC");
 
             }
 
@@ -25,67 +24,67 @@ public class EventManager {
             for (Event event: EventList ){
                 if(event.getRoomNumber().equals(roomNumber) && event.getEventTime().equals(eventTime)){
                     trigger = false;
-                    System.out.println("Error: Event has the following conflict:");
-                    System.out.println("Event Name: " + event.getEventName());
-                    System.out.println("Event Time: " + event.getEventTime());
-                    System.out.println("Room Number: " + event.getRoomNumber());
-                    break;
+                    return "EC";
                 }
             }
             if (trigger){
                 Event newEvent = new Event(eventName, speakerName, eventTime, roomNumber, attendeeList);
                 EventList.add(newEvent);
                 listOfTalks.put(eventTime, eventName);
-                check = true;
+                return "YES";
             }
         }
-        return check;
+        return null;
 
 
     }
 
-    public boolean removeEvent(String eventName, HashMap<String, String> listOfTalks ){
+    public String removeEvent(String eventName, HashMap<String, String> listOfTalks ){
         //Need to still remove it from attendee's list of events
         Event event = getEvent(eventName);
-        boolean check = false;
         if (EventList.contains(event) && event !=null){
             EventList.remove(event);
             listOfTalks.remove(event.getEventTime(),event.getSpeakerName());
-            check = true;
+            return "YES";
         }
         else{
-            System.out.println("Event does not exist; cannot remove event");
+            return("EDE");
         }
-        return check;
     }
 
-    public boolean reserveAttendee(String eventName, String UserId){
+    public String reserveAttendee(String eventName, String UserId, ArrayList<String> eventsAttending){
         //Need to check if attendee is not already registered for event at this time
         Event event = getEvent(eventName);
-        boolean check = false;
-        if (event.getAttendeeList().size() <2 && event!=null){
+        if (event.getAttendeeList().size() <2 && event!=null && !eventsAttending.contains(eventName)){
             event.getAttendeeList().add(UserId);
-            check = true;
+            eventsAttending.add(eventName);
+            return "YES";
 
         }
-        else{
-            System.out.println("Cannot reserve spot due to full capacity of event.");
+        else if(event.getAttendeeList().size() >=2){
+            return("FCE");
         }
-        return check;
+        else if(event == null){
+            return("EDE");
+        }
+        else if(eventsAttending.contains(eventName)){
+            return "AAE";
+        }
+        return null;
+
     }
 
-    public boolean removeAttendee(String eventName, String UserId){
+    public String removeAttendee(String eventName, String UserId, ArrayList<String> eventsAttending){
         Event event = getEvent(eventName);
-        boolean check = false;
         if(event.getAttendeeList().contains(UserId) && event!=null){
             event.getAttendeeList().remove(UserId);
-            check = true;
+            eventsAttending.remove(eventName);
+            return "YES";
 
         }
         else{
-            System.out.println("Error: Cannot find user at this event");
+            return("ADE");
         }
-        return check;
 
     }
 
@@ -96,7 +95,6 @@ public class EventManager {
                 return event;
             }
         }
-
         return null;
 
     }
