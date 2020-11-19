50f678c150f8813bd8b56da39fb9b4262b691e43
akshatayush <akshatayush07@gmail.com>
2020-11-20 00:40:44 +0530

    Main.java added to run the application

---
+++ phase1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    /**
+     * main method to run the program
+     * @param args: string arguments for main method
+     */
+    public static void main(String[] args) {
+        MasterSystem masterSystem = new MasterSystem();
+        masterSystem.run();
+    }
+}
