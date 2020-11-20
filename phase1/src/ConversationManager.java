856722a5ca299a6f07794a99fcc2de977220639c
PRB27 <peter.bilski.1@gmail.com>
2020-11-19 21:19:49 -0500

    Finally finished UserMessageController

--- phase1/src/ConversationManager.java
+++ phase1/src/ConversationManager.java
@@ -11,34 +11,21 @@ public class ConversationManager {
         return participantList;
     }
 
-    //creates a conversation via a message
-    public Conversation createNewConversation(Message originalMessage){
-        ArrayList<String> participants = participants(originalMessage);
-
-        Conversation convo = new Conversation(participants, originalMessage.getId());
-
-        allConversations.add(convo);
-
-        return convo;
-    }
-
     //creates a conversation via participants
-    public Conversation createNewConversation(ArrayList<String> participants){
+    public String createNewConversation(ArrayList<String> participants){
 
         Conversation convo = new Conversation(participants, "");
 
         allConversations.add(convo);
 
-        return convo;
+        return convo.getId();
     }
 
-    //either returns an existing convo with participants or creates a new one with those participants
-    public Conversation getConvoByParticipants(ArrayList<String> participants){
+    public void setConvoRoot(String convoId, String messageId) {
         for(Conversation c: allConversations){
-            if(c.getParticipants().equals(participants)){
-                return c;
+            if(c.getId() == convoId){
+                c.setConvoRoot(messageId);
             }
         }
-        return createNewConversation(participants);
     }
 }
