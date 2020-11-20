dac37bd7e6713ea0db82f01a163a65b5cd71e7ba
ASWIN <ashwin.karthikeyan@mail.utoronto.ca>
2020-11-20 13:28:17 -0500

    javadoc

--- phase1/src/Organizer.java
+++ phase1/src/Organizer.java
@@ -17,10 +17,18 @@ public class Organizer extends User implements Serializable {
 
     }
 
+    /**
+     * returns an ArrayList of event titles of events that this Organizer object is attending
+     * @return ArrayList of events that this Organizer object is attending
+     */
     public ArrayList<String> getEventsAttending() {
         return eventsAttending;
     }
 
+    /**
+     * Stores a list of event titles that this Organizer object is attending in this object
+     * @param eventsAttending : A list of event titles that this Organizer object is attending (param_type :ArrayList<String>)
+     */
     public void setEventsAttending(ArrayList<String> eventsAttending) {
         this.eventsAttending = eventsAttending;
     }
