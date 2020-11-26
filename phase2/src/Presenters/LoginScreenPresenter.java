86c68748f668d62d22df668b15febcbbb527a3aa
vlad <vladimir_c10@hotmail.com>
2020-11-26 13:41:11 -0500

    made some changes

--- phase2/src/Presenters/LoginScreenPresenter.java
+++ phase2/src/Presenters/LoginScreenPresenter.java
@@ -1,4 +1,42 @@
 package Presenters;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+
 public class LoginScreenPresenter {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private HBox Username;
+    @FXML
+    private HBox Password;
+    @FXML
+    private Button loginButton;
+
+
+    @FXML
+    private void initialize(){
+        //search panel
+        loginButton.setText("Login");
+        loginButton.setOnAction(event -> callUserMenu());
+        loginButton.setStyle("-fx-background-color: #457ecd; -fx-text-fill: #ffffff;");
+
+        loginButton.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER)) {
+                callUserMenu();
+            }
+        });
+    }
+    @FXML
+    private void callUserMenu(){
+
+    }
 }
