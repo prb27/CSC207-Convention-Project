983d2d3281c18eab314e1cc4105c3449cc051b8e
vlad <vladimir_c10@hotmail.com>
2020-12-02 19:10:35 -0500

    edited SignUpMenuController and SignUpMenuPresenter

--- phase2/src/Controllers/SignUpMenuController.java
+++ phase2/src/Controllers/SignUpMenuController.java
@@ -1,4 +1,25 @@
 package Controllers;
 
+import Main.Main;
+import Presenters.SignUpMenuPresenter;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
 public class SignUpMenuController {
+
+    private Main main1;
+    private AccountHandler accountHandler;
+
+    public void returnToLogin() throws Exception {
+        FXMLLoader loader = new FXMLLoader(SignUpMenuPresenter.class.getResource("/LoginMenuView.fxml"));
+        AnchorPane loginPage = (AnchorPane) loader.load();
+        Scene loginPageScene = new Scene(loginPage);
+
+        main1.getStage().setScene(loginPageScene);
+    }
+
+
+
+
 }
--- phase2/src/Presenters/SignUpMenuPresenter.java
+++ phase2/src/Presenters/SignUpMenuPresenter.java
@@ -4,7 +4,6 @@ import Controllers.LoginMenuController;
 import Controllers.SignUpMenuController;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
@@ -17,7 +16,7 @@ import java.io.IOException;
 public class SignUpMenuPresenter  {
 
     private SignUpMenuController signUpMenuController;
-    private Main main1;
+
     @FXML
     private TextField createUsername;
     @FXML
@@ -45,8 +44,8 @@ public class SignUpMenuPresenter  {
         loginFromSignUp.setText("Login");
         loginFromSignUp.setOnAction(event -> {
             try {
-                returnToLogin();
-            } catch (IOException e) {
+                signUpMenuController.returnToLogin();
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         });
@@ -54,23 +53,15 @@ public class SignUpMenuPresenter  {
         loginFromSignUp.setOnKeyPressed(event -> {
             if (event.getCode().equals(KeyCode.ENTER)) {
                 try {
-                    returnToLogin();
-                } catch (IOException e) {
+                    signUpMenuController.returnToLogin();
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
     }
 
-    private void returnToLogin() throws IOException {
-        FXMLLoader loader = new FXMLLoader(SignUpMenuPresenter.class.getResource("/LoginMenuView.fxml"));
-        AnchorPane loginPage = (AnchorPane) loader.load();
-        Scene loginPageScene = new Scene(loginPage);
 
-        main1.getStage().setScene(loginPageScene);
-
-
-    }
     private void createUser(){
 
         }
