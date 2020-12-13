7420b5242e237583d6cff873a34a8333c7778a7b
ASWIN <ashwin.karthikeyan@mail.utoronto.ca>
2020-12-13 16:47:54 -0500

    EventsSearchEngine Javadoc complete

--- phase2/src/Controllers/EventsSearchEngine.java
+++ phase2/src/Controllers/EventsSearchEngine.java
@@ -6,7 +6,10 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * This class contains methods that perform actions like searching for events with certain parameters (eg. Speaker's username).
+ * @author Ashwin
+ */
 public class EventsSearchEngine implements Serializable {
 
     private final EventManager eventManager;
@@ -17,6 +20,12 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events with speaker </speakername>
+     * @param speakerName the speaker's username (param_type: String)
+     * @return List</String>
+     */
     public List<String> eventsWithSpeaker(String speakerName){
 
         List<String> eventsWithThisSpeaker = new ArrayList<>();
@@ -29,7 +38,12 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
-
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events with start time </startTime>
+     * @param startTime the start time for the searched event (param_type: String)
+     * @return List</String>
+     */
     public List<String> eventsAtStartTime(String startTime){
 
         List<String> eventsAtThisStartTime = new ArrayList<>();
@@ -42,7 +56,12 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
-
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events that are duration </duration> hours long.
+     * @param duration the duration of the events being search for (param_type: int)
+     * @return List</String>
+     */
     public List<String> eventsForThisDuration(int duration){
 
         List<String> eventsForThisDuration = new ArrayList<>();
@@ -55,7 +74,13 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
-
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events with speaker </speakername> and duration </duration>
+     * @param speakerName username of the speaker for the searched event (param_type: String)
+     * @param duration duration of the searched event (param_type: int)
+     * @return List</String>
+     */
     public List<String> eventsWithSpeakerAndDuration(String speakerName, int duration){
 
         List<String> requiredEvents = new ArrayList<>();
@@ -70,6 +95,13 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events with speaker </speakername> at start time </time>
+     * @param speakerName username of the speaker for the searched event (param_type: String)
+     * @param time start time of the searched event (param_type: String)
+     * @return List</String>
+     */
     public List<String> eventWithSpeakerAndStartTime(String speakerName, String time){
 
         List<String> requiredEvents = new ArrayList<>();
@@ -84,6 +116,13 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events at start time </time> and duration </duration>
+     * @param time start time of the searched event (param_type: String)
+     * @param duration duration of the searched event (param_type: int)
+     * @return List</String>
+     */
     public List<String> eventsWithStartTimeAndDuration(String time, int duration){
 
         List<String> requiredEvents = new ArrayList<>();
@@ -98,6 +137,14 @@ public class EventsSearchEngine implements Serializable {
 
     }
 
+    /**
+     * Returns a List of Strings that provide information about the event title, start time, duration, roomId and Speaker names
+     * for events with speaker </speakername> at start time </time> and duration </duration> hours.
+     * @param speakerName username of the speaker for the searched event (param_type: String)
+     * @param time start time of the searched event (param_type: String)
+     * @param duration duration of the searched event (param_type: int)
+     * @return List</String>
+     */
     public List<String> eventWithSpeakerNameStartTimeAndDuration(String speakerName, String time, int duration){
 
         List<String> requiredEvents = new ArrayList<>();
