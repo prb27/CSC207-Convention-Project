014bd62e9ee0a62e30a62b96459405e25a069d4e
akshatayush <akshatayush07@gmail.com>
2020-12-14 13:38:43 +0530

    database update

--- phase2/src/Gateways/AttendeeDatabase.java
+++ phase2/src/Gateways/AttendeeDatabase.java
@@ -8,8 +8,11 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
 
 public class AttendeeDatabase extends UserDatabase implements IAttendeeDatabase{
 
@@ -19,23 +22,29 @@ public class AttendeeDatabase extends UserDatabase implements IAttendeeDatabase{
 
 
     @Override
-    public List<Map<String, String>> getAttendees(){
-        // We start by getting the collection we want. In this case, the attendee. We have already instantiated it above.
-        // Now, for our collection, we store documents of data related to the attendee entity. Thus, we must
-        // store every document in a data structure. Let's use a list.
-
-        //userType
-        DBCursor cursor = userCollection.find();
-
-        while(cursor.hasNext()){
+    public List<Map<String, List<String>>> getAttendees(){
+        List<Map<String, List<String>>> attendeeList = new ArrayList<>();
+        List<Document> attendeeDocuments = userCollection.find(eq("userType", "attendee")).into(new ArrayList<>());
+        for(Document attendeeDocument: attendeeDocuments) {
+            Map<String, List<String>> attendee = new HashMap<>();
+            List<String> credentials = new ArrayList<>();
+            String username = attendeeDocument.getString("username");
+            credentials.add(username);
+            credentials.add(attendeeDocument.getString("password"));
+            attendee.put("credentials", credentials);
+            attendee.put("contacts", getContactsForUser(username));
+            attendee.put("conversations", getConversationsForUser(username));
+            attendee.put("eventsAttending", getEventsAttendingForUser(username));
+            attendeeList.add(attendee);
         }
-
-
+        return attendeeList;
     }
 
-    @Override
-    public List<String> getEventsAttending(String attendee) {
-        return null;
+    public List<String> getEventsAttendingForUser(String username) {
+        List<Document> user = userCollection.find(eq("username", username)).into(new ArrayList<>());
+        @SuppressWarnings("unchecked")
+        List<String> eventsAttending = (ArrayList<String>) user.get(0).get("conversations");
+        return eventsAttending;
     }
 
 }
